0c95b4550889c46062589027d3467f46a41d65e0
Christopher Zinn <tczinn@gmail.com>
2020-10-24 10:07:22 +0200

    added custom rate limiting

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rateLimit: {
-    windowMs: 60 * 60 * 1000, // 1 hour
-    max: 5, // limit each IP to 5 requests per windowMs
+    windowMs: 1000, // 1 second
+    max: 10, // limit each IP to 10 requests per windowMs
   },
 };
 
